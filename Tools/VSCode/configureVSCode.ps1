04428c483c274d7b87a8bfd2a34d39d7aac3fbd9
Kyle Burk <kyleburk@live.com>
2020-04-05 18:37:29 -0500

    Fix VSCode RegKey

--- Tools/VSCode/configureVSCode.ps1
+++ Tools/VSCode/configureVSCode.ps1
@@ -4,7 +4,7 @@ if($env:OS -eq 'Windows_NT'){
     if(!(Test-Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers")){
         New-Item -Path 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers'
     }
-    Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers' -Name "C:\\Program Files\\Microsoft VS Code\\Code.exe" -Value "~ RUNASADMIN" | Out-Null
+    Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers' -Name "C:\Program Files\Microsoft VS Code\Code.exe" -Value "~ RUNASADMIN" | Out-Null
 }
 #install the VSCode Powershell Extension
 code --install-extension ms-vscode.powershell
